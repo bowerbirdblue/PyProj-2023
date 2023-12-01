ac9b2ccd1b22665fc7af2fba62e7943060f4ecc8
bowerbirdblue <148992515+bowerbirdblue@users.noreply.github.com>
2023-12-01 11:23:08 -0500

    Update pyproj 23.py

--- pyproj 23.py
+++ pyproj 23.py
@@ -1,1 +1,10 @@
 # universal serial 🅱️us
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import ttk
+from tkinter import messagebox
+# creating the login (+submit/cancel buttons)
+def submit_login():
+    username = username_entry.get()
+    password = username_entry.get()
+    if username == "Lanius.ludovicianus.to" and password == "ehk.inh.corv.0.111.00.11":
