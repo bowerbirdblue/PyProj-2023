9531b94ff1ede2bd72f9a026889088ba171ffadc
bowerbirdblue <148992515+bowerbirdblue@users.noreply.github.com>
2023-12-01 11:34:44 -0500

    Update pyproj 23.py

--- pyproj 23.py
+++ pyproj 23.py
@@ -3,8 +3,13 @@ import tkinter as tk
 from tkinter import simpledialog
 from tkinter import ttk
 from tkinter import messagebox
+def open_app_win():
+    app_win = tk.Tk()
+    app_win.title("")
 # creating the login (+submit/cancel buttons)
 def submit_login():
     username = username_entry.get()
     password = username_entry.get()
-    if username == "Lanius.ludovicianus.to" and password == "ehk.inh.corv.0.111.00.11":
+    if username == "gid.l.ehk.tom" and password == "alcid.corv.0.111.00.11":
+        messagebox.showinfo("Login Successful.", "Welcome to the Tomia.")
+
